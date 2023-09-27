9cabbb3201bf7a47a4f6f24bf23bb9af677744cb
delschadm <delschadm@gmail.com>
2023-09-28 00:04:33 +0200

    test

--- Easy/TwoSum.py
+++ Easy/TwoSum.py
@@ -4,28 +4,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 1. 暴力法
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
+        return [nums.index(target - n), i] if target - n in nums else None
 
-        # 2. 两遍哈希表
-        # hash_table = {}
-        # for i in range(len(nums)):
-        #     hash_table[nums[i]] = i
-        # for i in range(len(nums)):
-        #     complement = target - nums[i]
-        #     if complement in hash_table and hash_table[complement] != i:
-        #         return [i, hash_table[complement]]
-
-        # 3. 一遍哈希表
-        hash_table = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hash_table:
-                return [hash_table[complement], i]
-            hash_table[nums[i]] = i
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
